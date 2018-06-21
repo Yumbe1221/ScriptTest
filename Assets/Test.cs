e6a628a4ad2ef17d20f0981ee01e3b5ee5bc3c86
Hirona Yumbe <hirona.yumbe@fujifilm.com>
2018-06-21 19:57:42 +0900

    ScriptTest 課題提出

---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] points = new int[5];
+
+        // 配列の各要素に値を代入する
+        points[0] = 10;
+        points[1] = 20;
+        points[2] = 30;
+        points[3] = 40;
+        points[4] = 50;
+
+        // 配列の要素をすべて表示する
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(points[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
