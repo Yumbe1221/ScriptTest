4ced873ac91885fbf74f243b6095be1aa5113971
Hirona Yumbe <hirona.yumbe@fujifilm.com>
2018-07-05 20:36:09 +0900

    課題再提出

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,29 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class Boss
+{
+    private int mp = 53;  // mp
+
+    // 魔法攻撃用の関数
+    public void Magic(int damage)
+    {
+        if (mp < damage)
+        {
+            Debug.Log("MPが足りないため魔法が使えない。" );
+        }
+        else
+        {
+            // 残りmpを減らす
+            this.mp -= damage;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+        }
+
+    }
+
+
+
+}
+
 public class Test : MonoBehaviour
 {
 
-    // Use this for initialization
     void Start()
     {
-        // 要素数5の配列を初期化する
-        int[] points = new int[5];
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
 
-        // 配列の各要素に値を代入する
-        points[0] = 10;
-        points[1] = 20;
-        points[2] = 30;
-        points[3] = 40;
-        points[4] = 50;
+        
 
-        // 配列の要素をすべて表示する
-        for (int i = 4; i >= 0; i--)
+
+        // 10回処理を繰り返す
+        for (int i = 0; i < 11; i++)
         {
-            Debug.Log(points[i]);
+            // 魔法攻撃用の関数を呼び出す
+            lastboss.Magic(10);
         }
     }
-
+      
+   
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Test2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int array = { 10, 20, 30, 40, 50 };
+
+        // 配列の要素をすべて昇順で表示する
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        // 配列の要素をすべて降順で表示する
+        for (int i = array.Length-1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
